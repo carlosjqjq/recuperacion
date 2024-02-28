a8004193f9fcc1e7c01f479a657772f0bd213cd0
carlosjqjq <108626136+carlosjqjq@users.noreply.github.com>
2024-02-28 10:41:43 -0500

    Add files via upload

---
+++ libreria/capa_datos2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_datos2
+{
+    public class Class1
+    {
+    }
+}
